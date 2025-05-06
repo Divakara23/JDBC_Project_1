d3236038fb72f8ff84dd2b1ab24ddc5d31649423
lochana d <lochanagowda456@gmail.com>
2025-05-06 09:31:55 +0530

    Update Program1.java

--- src/com/Test/Program1.java
+++ src/com/Test/Program1.java
@@ -1,9 +1,13 @@
 package com.Test;
-
+import java.util.Scanner;
 public class Program1 {
 
 	public static void main(String[] args) {
-System.out.println("Hello world");
+		Scanner sc=new Scanner(System.in)
+		System.out.println("Enter the number");
+		int num=sc.nextInt();
+		res=num*num;
+		System.out.println("res"+res);
 	}
 
 }
